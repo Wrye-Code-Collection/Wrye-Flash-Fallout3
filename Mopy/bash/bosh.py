215acc73a26cace9c304b2413f903a307aa23c44
Sharlikran <danielhmpdx16@gmail.com>
2014-08-03 03:46:31 -0700

    Comment added to MelOwnership

--- Mopy/bash/bosh.py
+++ Mopy/bash/bosh.py
@@ -1449,6 +1449,7 @@ class MelOwnership(MelGroup):
         MelGroup.__init__(self, 'ownership',
             MelFid('XOWN','owner'),
             MelOptStruct('XRNK','i',('rank',None)),
+            # Double check XGLB it's not used in FO3Edit
             MelFid('XGLB','global'),
         )
 

60fc446d0f6c9516534c82eb4a76db6b674ca683
Sharlikran <danielhmpdx16@gmail.com>
2015-01-11 23:29:48 -0800

    Updated MreFlst

--- Mopy/bash/bosh.py
+++ Mopy/bash/bosh.py
@@ -3345,7 +3345,7 @@ class MreFlst(MelRecord):
     classType = 'FLST'
     melSet = MelSet(
         MelString('EDID','eid'),
-        MelFids('LNAM','fids'),
+        MelFids('LNAM','formIDInList'),
         )
     __slots__ = (MelRecord.__slots__ + melSet.getSlotsUsed() +
         ['mergeOverLast','mergeSources','items','deflsts'])
@@ -3356,12 +3356,13 @@ class MreFlst(MelRecord):
         self.mergeOverLast = False #--Merge overrides last mod merged
         self.mergeSources = None #--Set to list by other functions
         self.items  = None #--Set of items included in list
-        self.deflsts = None #--Set of items deleted by list (Deflst mods)
+        #--Set of items deleted by list (Deflst mods) unused for Skyrim
+        self.deflsts = None
 
     def mergeFilter(self,modSet):
         """Filter out items that don't come from specified modSet."""
         if not self.longFids: raise StateError(_("Fids not in long format"))
-        self.fids = [fid for fid in self.fids if fid[0] in modSet]
+        self.formIDInList = [fid for fid in self.formIDInList if fid[0] in modSet]
 
     def mergeWith(self,other,otherMod):
         """Merges newLevl settings and entries with self.
@@ -3371,27 +3372,25 @@ class MreFlst(MelRecord):
         #--Remove items based on other.removes
         if other.deflsts:
             removeItems = self.items & other.deflsts
-            #self.entries = [entry for entry in self.entries if entry.listId not in removeItems]
-            self.fids = [fid for fid in self.fids if fid not in removeItems]
+            self.formIDInList = [fid for fid in self.formIDInList if fid not in removeItems]
             self.items = (self.items | other.deflsts)
         hasOldItems = bool(self.items)
         #--Add new items from other
         newItems = set()
-        fidsAppend = self.fids.append
+        formIDInListAppend = self.formIDInList.append
         newItemsAdd = newItems.add
-        for fid in other.fids:
+        for fid in other.formIDInList:
             if fid not in self.items:
-                fidsAppend(fid)
+                formIDInListAppend(fid)
                 newItemsAdd(fid)
         if newItems:
             self.items |= newItems
-            #self.fids.sort(key=attrgetter('level'))
-            self.fids.sort
+            self.formIDInList.sort
         #--Is merged list different from other? (And thus written to patch.)
-        if len(self.fids) != len(other.fids):
+        if len(self.formIDInList) != len(other.formIDInList):
             self.mergeOverLast = True
         else:
-            for selfEntry,otherEntry in zip(self.fids,other.fids):
+            for selfEntry,otherEntry in zip(self.formIDInList,other.formIDInList):
                 if selfEntry != otherEntry:
                     self.mergeOverLast = True
                     break
@@ -33816,7 +33815,7 @@ class FidListsMerger(SpecialPatcher,ListPatcher):
             for type in self.listTypes:
                 for levList in getattr(modFile,type).getActiveRecords():
                     masterItems = self.masterItems.setdefault(levList.fid,{})
-                    masterItems[modName] = set(levList.fids)
+                    masterItems[modName] = set(levList.formIDInList)
             self.mastersScanned.add(modName)
         #--Deflst setup
         configChoice = self.configChoices.get(modName,tuple())
@@ -33829,7 +33828,7 @@ class FidListsMerger(SpecialPatcher,ListPatcher):
                 listId = newLevList.fid
                 isListOwner = (listId[0] == modName)
                 #--Items, deflsts sets
-                newLevList.items = items = set(newLevList.fids)
+                newLevList.items = items = set(newLevList.formIDInList)
                 if not isListOwner:
                     #--Deflsts: all items in masters minus current items
                     newLevList.deflsts = deflsts = set()
